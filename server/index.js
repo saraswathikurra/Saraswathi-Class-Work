cffb2127120c7a5a7cb194d2e2012fa52263ff8b
kurra <sarsukurra11@gmail.com>
2020-04-06 02:47:00 -0400

    server index

--- server/index.js
+++ server/index.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const path = require('path');
 
-const quoteCards = require('./game/quoteCards');
+const gameController = require('./controllers/game');
+const usersController = require('./controllers/users');
+
+
 
 const app = express();
 const port = 3000;
 
 app
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use(express.static( __dirname + '/../client/dist'))
     .get('/', (req, res) => res.send('This class is awesome!') )
-    .get('/game/quoteCards', (req, res) => res.send(quoteCards) );
+    .use('/game', gameController)
+    .use('/users', usersController)
 
-
+    .use((req, res) => {
+        const homePath = path.join( __dirname , '/../client/dist/index.html');
+        console.log(homePath);
+        res.sendFile(homePath)
+    })
+    .use( (err, req, res, next ) => {
+        console.error(err);
+        const errorCode = err.code || 500;
+        res.status(errorCode).send({ message: err.message });
+    } )
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
